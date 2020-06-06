84dc22035b97632d8cc114e4efb4354e2aecab3a
Sihle Kambula <kambulasihle@gamil.com>
2020-06-06 16:38:07 +0200

    sql query fix

--- queries.sql
+++ queries.sql
@@ -1,15 +1,20 @@
 CREATE DATABASE registration;
-USE acme;
+USE registration;
 CREATE TABLE users
 (
-    id INT,
-    firstName VARCHAR(255),
-    lastName VARCHAR(255),
-    email VARCHAR(255),
+    id INT
+    AUTO_INCREMENT,
+    firstName VARCHAR
+    (255),
+    lastName VARCHAR
+    (255),
+    email VARCHAR
+    (255),
     cellNumber INT,
-    password VARCHAR(255),
+    password VARCHAR
+    (255),
 );
 
-INSERT INTO users
-    (firstName,lastName,email,cellNumber,userPassword)
-VALUES('$firstName', '$lastName', '$email', '$cellNumber', '$password');
+    INSERT INTO users
+        (firstName,lastName,email,cellNumber,userPassword)
+    VALUES('$firstName', '$lastName', '$email', '$cellNumber', '$password');
